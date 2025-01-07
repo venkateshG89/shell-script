8eecf6a1c7aa11a0b93601da95fd5a2a7b9280b3
venkateshG89 <venkygari66@gmail.com>
2025-01-08 04:21:14 +0530

    conv

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PERSON1=$1
-PERSON2=S$2
+PERSON2=$2
 echo " $PERSON1: hi $PERSON2"
 echo " $PERSON2: hi $PERSON1"
 echo " $PERSON1: how r u doing"
---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo " Please enter employee name "
+
+read -s EMPLOYEE NAME
+
+echo " Entered employee name is : $EMPLOYEE NAME "
+
+
